1f233efb108c67daea8906def5b92692f98651da
Erik Peteri <peteri.erik@gmail.com>
2023-12-31 18:01:41 +0200

    add cors

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import morgan from "morgan";
+var cors = require('cors')
 import { createProxyMiddleware } from 'http-proxy-middleware';
 require('dotenv').config()
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const api_key = process.env.API_KEY;
 const api_base_url = process.env.API_BASE_URL;
